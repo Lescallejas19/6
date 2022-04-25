bf2feaba8a718f3a454948d3249fbd647502241b
Lescallejas19 <101581325+Lescallejas19@users.noreply.github.com>
2022-04-25 09:08:55 -0500

    6
    6

---
+++ Product.js
@@ -0,0 +1,16 @@
+/**
+ * Product Class
+ */
+export class Product {
+ /**
+  *
+  * @param {string} Nombre The Product Name
+  * @param {number} Producto The Product Price
+  * @param {number} Año The year creation of the Product
+  */
+ constructor(name, price, year) {
+  this.name = name
+  this.price = price
+  this.year = year
+ }
+}
